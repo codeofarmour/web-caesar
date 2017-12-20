d32e219c0dc5f3eb1f4b467eee54c2d8a9bcb8eb
Terri Riggle <codeofarmour@outlook.com>
2017-12-20 02:36:24 -0500

    added validations for integer and text inputs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from caesar import rotate_string
+import cgi
 
 app =  Flask(__name__)
 app.config['DEBUG'] = True
@@ -27,24 +28,60 @@ form = """
         </head>
         <body>
             <form action="/" method="post">
+                <p class="error">{rot_error}</p>
                 <label>Rotate by:<input name="rot" type="text" value=0 /></label>
-                <textarea name="text">{0}</textarea>
+                <textarea name="text">{text}</textarea>
                 <button>Submit</button>
             </form>
         </body>
     </html>
 """
 
+def is_integer(rot):
+    try:
+        int(rot)
+        return True
+    except ValueError:
+        return False
+
+def is_alpha(text):
+    try:
+        str(text)
+        return True
+    except ValueError:
+        return False
+
 @app.route("/")
 def index():
-    return form.format("")
+    return form.format(rot_error="", text="")
 
 @app.route("/", methods=["post", "get"])
+
 def encrypt():
-    rot = int(request.form["rot"])
+    rot = request.form["rot"]
     text = request.form["text"]
 
-    rotated = rotate_string(text, rot)
-    return form.format(rotated)
+    rot_error = ""
+    text_error = ""
+
+    if not is_integer(rot):
+        rot_error = "Numbers only please"
+        rot = ""
+        return form.format(rot_error=rot_error, text="")
+    else:
+        rot = int(rot)
+        text = cgi.escape(text)
+        rotated = rotate_string(text, rot)
+        return form.format(rot_error="", text_error="")
+    
+    if not is_alpha(text):    
+        text_error = "Letters only please"
+        text = ""
+        return form.format(text_error=text_error, rot="")
+    else:
+        text = str(text)
+        text = cgi.escape(text)
+        rotated = rotate_string(text, rot)
+        return form.format(rot_error="", text_error="")   
 
 app.run()
